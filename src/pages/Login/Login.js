d779b424b474c427aa8f36c2ffb7ed9f1126b494
sojin lee <sojin626@naver.com>
2023-05-02 19:28:47 +0900

    Add : 로그인 레이아웃

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
-  return <div className="login">로그인</div>;
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/');
+  };
+
+  const goToSignup = () => {
+    navigate('/signup');
+  };
+
+  return (
+    <div className="loginform">
+      <p className="start">
+        <strong>로그인 및 회원가입</strong>을<p>시작합니다.</p>
+      </p>
+      <div className="inputwrap">
+        <input
+          className="login"
+          type="text"
+          placeholder="이메일을 입력해주세요"
+        />
+        <input
+          className="password"
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+        />
+        <div className="id-check">
+          <input type="radio" id="id-check" name="id-check" value="id-check" />
+          <label for="id-check">아이디 저장</label>
+        </div>
+      </div>
+      <button className="loginbtn" type="button" onClick={goToMain}>
+        로그인
+      </button>
+      <button className="signupbtn" type="button" onClick={goToSignup}>
+        회원가입
+      </button>
+      <div className="forget">
+        <a href="/forgetid" className="forgetid">
+          이메일 찾기
+        </a>
+        <a href="/forgetpassword" className="forgetpassword">
+          비밀번호 찾기
+        </a>
+      </div>
+    </div>
+  );
 };
+
 export default Login;
