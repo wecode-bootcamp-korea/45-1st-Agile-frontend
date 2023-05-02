9da2f8ab7a78af59fb6be9a6d30bb1055d5b4651
김준섭 <junsobi@gmail.com>
2023-05-02 17:14:49 +0900

    Add : Header, Nav, Footer components 업로드

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => {
-  return <footer className="footer">footer</footer>;
+  return (
+    <div className="footer">
+      <div className="realfooter"> 푸터입니다</div>
+    </div>
+  );
 };
 export default Footer;
--- src/components/Nav/Nav-main.js
+++ src/components/Nav/Nav-main.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav-main.scss';
 
 const NavMain = () => {
-  return <nav className="nav-main">nav-main</nav>;
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 150 && !isScrolled) {
+        setIsScrolled(true);
+      } else if (scrollTop <= 150 && isScrolled) {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isScrolled]);
+
+  return (
+    <div className="nav-main">
+      <div className={isScrolled ? 'nav-contaner-scrolled' : 'nav-contaner'}>
+        <div className="nav-main-thing">
+          <div className="nav-main-left">
+            <img
+              alt="책"
+              className="viewmore"
+              src="../images/components/nav/books.png"
+            />
+            <div className="category">카테고리</div>
+          </div>
+          <div className="nav-main-right">
+            <input className="search-bar" type="text" />
+            <img
+              className="heart"
+              alt="heart"
+              src="../images/components/nav/heart.png"
+            />
+            <img
+              alt="쇼핑백"
+              className="shopingbag"
+              src="../images/components/nav/shopingbag.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
+
 export default NavMain;
