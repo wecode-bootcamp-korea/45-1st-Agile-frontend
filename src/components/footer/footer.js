93386f6df99040caf4f973b673f119dccc190b46
김준섭 <junsobi@gmail.com>
2023-05-02 13:48:50 +0900

    ADD : nav,footer 어느정도 외형기능 생성

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="realfooter"> 푸터입니다</div>
+    </div>
+  );
+};
+export default Footer;
---
+++ src/components/header/header.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './header.scss';
+
+function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(!!token); // 토큰이 있으면 true, 없으면 false
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };
+
+  const handleLoginClick = () => {
+    if (isLoggedIn) {
+      handleLogout();
+    }
+  };
+
+  return (
+    <div className="header">
+      <Link to={isLoggedIn ? '/mypage' : '/signup'}>
+        <button className="to-signup">
+          {isLoggedIn ? '마이페이지' : '회원가입'}
+        </button>
+      </Link>
+
+      <Link to={isLoggedIn ? '/' : '/login'}>
+        <button className="to-login" onClick={handleLoginClick}>
+          {isLoggedIn ? '로그아웃' : '로그인'}
+        </button>
+      </Link>
+
+      <Link to="/customer-center">
+        <button className="to-customer-center">고객센터</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Header;
