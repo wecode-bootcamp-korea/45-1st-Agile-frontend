9cb5f9bdeb8c29dd668d3f046d5dc75a618fc12e
김준섭 <junsobi@gmail.com>
2023-05-02 17:59:02 +0900

    Modify : word-break:keep-all 추가

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,105 @@
 import React from 'react';
 import './Main.scss';
-
+import Header from '../../components/Header/Header';
+import NavMain from '../../components/Nav/Nav-main';
+import Footer from '../../components/Footer/Footer';
 const Main = () => {
-  return <div className="main">메인</div>;
+  return (
+    <div className="main">
+      <Header />
+      <NavMain />
+      <div>
+        메인페이지 내용입니다
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        스크롤이 어느정도됨
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br /> <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br /> <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br /> <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br /> <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br /> <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 export default Main;
