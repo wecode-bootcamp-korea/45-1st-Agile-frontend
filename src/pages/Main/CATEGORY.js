647ffab3e4ed18223830d928674f8a5fc4a890af
김준섭 <junsobi@gmail.com>
2023-05-03 11:00:17 +0900

    Add : main 레이아웃 구현

---
+++ src/pages/Main/CATEGORY.js
@@ -0,0 +1,16 @@
+const CATEGORY = [
+  {
+    id: 1,
+    name: '구독하기',
+    img: 'subscribe.png',
+    address: '',
+  },
+  { id: 2, name: '고등참고서', img: 'highschool.png', address: '' },
+  { id: 3, name: '중등참고서', img: 'middleschool.png', address: '' },
+  { id: 4, name: '어학', img: 'language.png', address: '' },
+  { id: 5, name: '인적성/직무능력', img: 'test.png', address: '' },
+  { id: 6, name: '성공/처세', img: 'success.png', address: '' },
+  { id: 7, name: '인간관계', img: 'peoples.png', address: '' },
+];
+
+export default CATEGORY;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CATEGORY from './CATEGORY';
 import './Main.scss';
 
 const Main = () => {
-  return <div className="main">메인</div>;
+  return (
+    <div className="main">
+      <TitleLine />
+      <TitleNav />
+      <TitleAd />
+      <TitleCategory />
+    </div>
+  );
 };
 export default Main;
+
+const TitleLine = () => {
+  return (
+    <div className="title-line">
+      <img className="logo" alt="logo" src="/images/main/logo.png" />
+      <input className="search-bar" />
+      <div className="title-right">
+        <button className="to-wishlist" />
+        <button className="to-cart" />
+      </div>
+    </div>
+  );
+};
+
+const TitleNav = () => {
+  return (
+    <div className="title-nav">
+      <button className="menus" />
+      <div className="카테고리">카테고리</div>
+    </div>
+  );
+};
+
+const TitleAd = () => {
+  return (
+    <div className="title-ad">
+      <video autoPlay muted loop>
+        <source src="/images/main/main-ad1.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+};
+
+const TitleCategory = () => {
+  return (
+    <div className="title-category">
+      {CATEGORY.map(category => (
+        <Link key={category.id} to={category.address}>
+          <img src={`/images/main/${category.img}`} alt={category.name} />
+          <p>{category.name}</p>
+        </Link>
+      ))}
+    </div>
+  );
+};
