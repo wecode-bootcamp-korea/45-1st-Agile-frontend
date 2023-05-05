1db3904c93ff84b4b6998bc15cca0ba56ecf3bb6
오승민 <seungmin@oseungmin-ui-noteubug.local>
2023-05-05 21:34:57 +0900

    Add : 장바구니 기능 구현중

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Title from './Components/Title';
 import Product from './Components/Product';
 import OrderInfo from './Components/OrderInfo';
 import './Cart.scss';
 
 const Cart = () => {
+  const [product, setProductList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/data.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setProductList(data);
+      });
+  }, []);
+
   return (
     <div className="cart">
       <Title />
       <div className="content-wrap">
         <div className="checkbox-wrap">
-          <Product />
+          {product.map(item => (
+            <Product it={item} />
+          ))}
         </div>
         <div className="order-info">
           <OrderInfo />
--- src/pages/Cart/Components/Product.js
+++ src/pages/Cart/Components/Product.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import '../Cart.scss';
 
-const Product = () => {
+// 처음
+// const Product = (props) => {
+// console.log(props); // {it: {}}
+
+// 객체 구조 분해 후
+const Product = ({ it }) => {
+  // const { it } = props;
+  const { title } = it; // {{}}
+
+  // console.log(it);
   return (
     <div className="product-wrap">
       <div className="checkbox-inner">
         <input type="checkbox" className="choice-checkbox" />
         <div className="choice">전체선택(0/0)</div>
         <div className="divide">|</div>
-        <div className="choice">선택</div>
+        <div className="choice">선택삭제</div>
       </div>
       <div className="message-web">
-        <p>장바구니에 담긴 제품이 없습니다.</p>
+        {/* <p>장바구니에 담긴 제품이 없습니다.</p> */}
+        {title}
       </div>
-      s
     </div>
   );
 };
