cbe64d3b4a80b0d2efa7e0c2d43928f43c04ec8b
hyunah <qkrgusdk0303@naver.com>
2023-05-05 13:06:00 +0900

    Add: 결제페이지 기능구현 브랜치 생성 (이제 진짜 시작)

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import OrderList from './components/OrderList';
 import OrdererInfo from './components/Orderer';
 import Shipment from './components/Shipment';
