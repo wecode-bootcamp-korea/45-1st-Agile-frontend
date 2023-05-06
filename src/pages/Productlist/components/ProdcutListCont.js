6008319454805db779c032a8665b69bd22844d90
김준섭 <junsobi@gmail.com>
2023-05-07 00:22:10 +0900

    Modify : productlist의 Layout 완성

--- src/pages/Productlist/components/ProdcutListCont.js
+++ src/pages/Productlist/components/ProdcutListCont.js
@@ -3,9 +3,6 @@ import { Link } from 'react-router-dom';
 import './ProductListCont.scss';
 
 const ProductListCont = ({ subCategoryId }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [showCount, setShowCount] = useState(4);
-  const [extractedList, setExtractedList] = useState([]);
   const [visibleProducts, setVisibleProducts] = useState([]);
 
   useEffect(() => {
@@ -18,7 +15,7 @@ const ProductListCont = ({ subCategoryId }) => {
             book => book.sub_category_id === subCategoryId
           );
           const sortedBooks = filteredBooks.sort((a, b) => a.price - b.price);
-          setExtractedList(sortedBooks);
+          setVisibleProducts(sortedBooks);
         }
       } catch (error) {
         console.log(error);
@@ -27,66 +24,28 @@ const ProductListCont = ({ subCategoryId }) => {
     fetchData();
   }, [subCategoryId]);
 
-  useEffect(() => {
-    if (extractedList.length === 0) {
-      return;
-    }
-    setVisibleProducts(extractedList);
-  }, [extractedList]);
-
-  const handlePrevClick = event => {
-    event.preventDefault();
-    event.stopPropagation();
-    if (currentIndex > 0) {
-      setCurrentIndex(prevIndex => prevIndex - 1);
-    }
-  };
-
-  const handleNextClick = event => {
-    event.preventDefault();
-    event.stopPropagation();
-    if (currentIndex + showCount < extractedList.length) {
-      setCurrentIndex(prevIndex => prevIndex + 1);
-    }
-  };
-
   if (visibleProducts.length === 0) {
     return <div>Loading...</div>;
   }
-  return (
-    <div className="product-lineup">
-      <button className="product-slider-prev" onClick={handlePrevClick} />
-      <div className="product-slider-container">
-        <div
-          className="product-slider-track"
-          style={{
-            transform: `translateX(-${
-              currentIndex * (100 / extractedList.length)
-            }%)`,
-            width: `${100 * extractedList.length}%`,
-          }}
-        >
-          {visibleProducts.map(product => (
-            <Link key={product.id} to={`/books/${product.Key}`}>
-              <div
-                key={product.key}
-                className="product-item"
-                style={{ width: `${100 / extractedList.length}%` }}
-              >
-                <img
-                  className="product-img"
-                  src={`images/main/booksimg/${product.Key}.png`}
-                  alt={product.title}
-                />
-                <div className="product-title">{product.title}</div>
-                <div className="product-price">{product.price}원</div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
 
-      <button className="product-slider-next" onClick={handleNextClick} />
+  return (
+    <div className="product-list-cont">
+      <div className="product-list">
+        {visibleProducts.map((product, index) => (
+          <Link key={product.id} to={`/books/${product.Key}`}>
+            <div className="product-item">
+              <img
+                className="product-img"
+                src={`images/main/booksimg/${product.Key}.png`}
+                alt={product.title}
+              />
+              <div className="product-title">{product.title}</div>
+              <div className="product-price">{product.price}원</div>
+              <div className="product-subtitle">{product.subtitle}</div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
