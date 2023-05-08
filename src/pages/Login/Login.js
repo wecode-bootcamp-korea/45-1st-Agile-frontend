0ad87a0880c0240dd37ecf70a82b38f39603aaee
sojin lee <sojin626@naver.com>
2023-05-08 13:53:00 +0900

    Modify : 백엔드 통신 부분 수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,35 +5,36 @@ import './Login.scss';
 const Login = () => {
   const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState({
-    userId: '',
-    userPassword: '',
+    email: '',
+    password: '',
   });
 
-  const isinputValid = userInfo.userId.match(
-    `/^[a-z0-9]+@[a-z]+(.[a-z]{2,3}){1,2}$/`
+  const isInputValid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+    userInfo.userId
   );
 
   const handleInput = event => {
-    const { name, Info } = event.target;
-    setUserInfo({ ...userInfo, [name]: Info });
+    const { name, value } = event.target;
+    setUserInfo({ ...userInfo, [name]: value });
   };
 
-  const goToMain = () => {
-    if (!isinputValid) {
+  const goToMain = event => {
+    event.preventDefault();
+    if (isInputValid) {
       alert('이메일을 다시 입력해주세요!');
       return false;
-    } else if (!userInfo.userPassword) {
+    } else if (userInfo.userPassword) {
       alert('비밀번호를 입력해주세요!');
       return false;
     } else {
-      fetch('http://10.58.52.146:3000/users/logIn', {
+      fetch('http://10.58.52.230:3000/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json;charset=utf-8',
         },
         body: JSON.stringify({
-          email: userInfo.userId,
-          password: userInfo.userPassword,
+          email: userInfo.email,
+          password: userInfo.password,
         }),
       })
         .then(response => {
@@ -55,32 +56,37 @@ const Login = () => {
   return (
     <div className="loginform">
       <p className="start">
-        <strong>로그인 및 회원가입</strong>을<p>시작합니다.</p>
+        <strong>로그인 및 회원가입</strong>
+        <p>시작합니다.</p>
       </p>
-      <div className="inputwrap">
-        <input
-          className="login"
-          type="text"
-          placeholder="이메일을 입력해주세요"
-          onChange={handleInput}
-        />
-        <input
-          className="password"
-          type="password"
-          placeholder="비밀번호를 입력해주세요"
-          onChange={handleInput}
-        />
-        <div className="id-check">
-          <input type="checkbox" id="id-check" name="id-check" />
-          <label for="id-check">아이디 저장 </label>
+      <form>
+        <div className="inputwrap">
+          <input
+            className="login"
+            type="text"
+            name="email"
+            placeholder="이메일을 입력해주세요"
+            onChange={handleInput}
+          />
+          <input
+            className="password"
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력해주세요"
+            onChange={handleInput}
+          />
+          <div className="id-check">
+            <input type="checkbox" id="id-check" name="id-check" />
+            <label htmlFor="id-check">아이디 저장</label>
+          </div>
         </div>
-      </div>
-      <button className="loginbtn" type="button" onClick={goToMain}>
-        로그인
-      </button>
-      <button className="signupbtn" type="button" onClick={goToSignup}>
-        회원가입
-      </button>
+        <button className="loginbtn" type="submit" onClick={goToMain}>
+          로그인
+        </button>
+        <button className="signupbtn" type="button" onClick={goToSignup}>
+          회원가입
+        </button>
+      </form>
       <div className="forget">
         <div className="findemail">
           <Link to="/forgetemail">이메일 찾기</Link>
