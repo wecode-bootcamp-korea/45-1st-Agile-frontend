3745927182d37a8926d06ab201cfe45342cfbf23
hyunah <qkrgusdk0303@naver.com>
2023-05-09 11:16:22 +0900

    Add: 마이페이지 관심상품 체크박스 레이아웃 구현

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -7,7 +7,7 @@ import Likes from './components/Likes';
 import './Mypage.scss';
 
 const Mypage = () => {
-  const [menuMode, setMenuMode] = useState(1);
+  const [menuMode, setMenuMode] = useState(3);
   const menuList = {
     1: <OrderDelivery />,
     2: <Subscribes />,
--- src/pages/Mypage/components/Likes.js
+++ src/pages/Mypage/components/Likes.js
@@ -20,22 +20,27 @@ const Likes = () => {
         <ItemTitle
           title="관심 제품"
           details="관심 제품은 최대 200개까지 저장됩니다."
-          count={`총 ${likesList.length}개`}
         />
-        <div className="application-top">
-          <div className="text-lg application-details">
-            신청내역 ({likesList.length}건)
-          </div>
-          <div>선택삭제</div>
-        </div>
+
         <div className="likes-list">
-          {likesList.length === 0 && (
-            <div className="no-list">관심 상품이 없습니다.</div>
-          )}
-          {likesList.length !== 0 &&
-            likesList.map(data => {
-              return <LikesProduct key={data.id} data={data} />;
-            })}
+          <div className="likes-select">
+            <div className="all-select">
+              <input type="checkbox" />
+              <div className="text-lg">
+                <b>전체선택</b> (/{likesList.length})
+              </div>
+            </div>
+            <div>선택삭제</div>
+          </div>
+          <div className="likes-list-items">
+            {likesList.length === 0 && (
+              <div className="no-list">관심 상품이 없습니다.</div>
+            )}
+            {likesList.length !== 0 &&
+              likesList.map(data => {
+                return <LikesProduct key={data.id} data={data} />;
+              })}
+          </div>
         </div>
       </div>
     </div>
